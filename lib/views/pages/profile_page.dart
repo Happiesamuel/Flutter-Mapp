2d5347ce9900f2fc9503b7c96ca22390b1e15e2b
Happiesamuel <odionsamuel2005@gmail.com>
2026-07-16 06:27:35 +0100

    select image and take photo

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:fluttertest/data/notifiers.dart';
 import 'package:fluttertest/views/pages/welcome_page.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,11 +13,39 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final ImagePicker _picker = ImagePicker();
   TextEditingController controller = TextEditingController();
   bool? isChecked = false;
   bool? isSwitched = false;
   double slideValue = 0.0;
   String? menuItem = 'e1';
+  File? selectedImage;
+
+  Future<void> pickImageFromGallery() async {
+    try {
+      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        setState(() => selectedImage = File(image.path));
+      }
+    } catch (e) {
+      debugPrint('Image picker error: $e');
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Error: $e')));
+      }
+    }
+  }
+
+  Future<void> takePhoto() async {
+    final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+    if (photo != null) {
+      setState(() {
+        selectedImage = File(photo.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -23,7 +54,17 @@ class _ProfilePageState extends State<ProfilePage> {
         children: [
           CircleAvatar(
             radius: 50,
-            backgroundImage: AssetImage('assets/images/apple.jpeg'),
+            backgroundImage: selectedImage != null
+                ? FileImage(selectedImage!) as ImageProvider
+                : const AssetImage('assets/images/apple.jpeg'),
+          ),
+          FilledButton(
+            onPressed: () async => await pickImageFromGallery(),
+            child: Text('Select an Image'),
+          ),
+          FilledButton(
+            onPressed: () async => await takePhoto(),
+            child: Text('Take Photo'),
           ),
           ListTile(
             onTap: () {
--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -66,3 +66,97 @@ class _WidgetTreeState extends State<WidgetTree> {
     );
   }
 }
+// import 'package:flutter/material.dart';
+// import 'package:fluttertest/data/constants.dart';
+// import 'package:fluttertest/data/notifiers.dart';
+// import 'package:fluttertest/views/pages/home_page.dart';
+// import 'package:fluttertest/views/pages/profile_page.dart';
+// import 'package:fluttertest/views/pages/settings_page.dart';
+// import 'package:fluttertest/views/widgets/navbar_widget.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:swipeable_page_route/swipeable_page_route.dart';
+
+// List<Widget> pages = [HomePage(), ProfilePage()];
+
+// class WidgetTree extends StatefulWidget {
+//   const WidgetTree({super.key});
+
+//   @override
+//   State<WidgetTree> createState() => _WidgetTreeState();
+// }
+
+// class _WidgetTreeState extends State<WidgetTree> {
+//   final PageController _pageController = PageController();
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     selectedPageNotifer.addListener(_onNotifierChanged);
+//   }
+
+//   void _onNotifierChanged() {
+//     _pageController.animateToPage(
+//       selectedPageNotifer.value,
+//       duration: const Duration(milliseconds: 300),
+//       curve: Curves.easeInOut,
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     selectedPageNotifer.removeListener(_onNotifierChanged);
+//     _pageController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Flutter Mapp'),
+//         centerTitle: false,
+//         actions: [
+//           ValueListenableBuilder(
+//             valueListenable: isDarkModeNotifer,
+//             builder: (BuildContext context, dynamic value, Widget? child) {
+//               return IconButton(
+//                 onPressed: () async {
+//                   isDarkModeNotifer.value = !isDarkModeNotifer.value;
+//                   final SharedPreferences prefs =
+//                       await SharedPreferences.getInstance();
+//                   await prefs.setBool(
+//                     KConstants.themeKey,
+//                     isDarkModeNotifer.value,
+//                   );
+//                 },
+//                 icon: Icon(value ? Icons.light_mode : Icons.dark_mode),
+//               );
+//             },
+//           ),
+//           IconButton(
+//             onPressed: () {
+//               Navigator.push(
+//                 context,
+//                 SwipeablePageRoute(
+//                   builder: (context) {
+//                     return SettingsPage(title: 'Settings');
+//                   },
+//                 ),
+//               );
+//             },
+//             icon: Icon(Icons.settings),
+//           ),
+//         ],
+//       ),
+//       body: PageView(
+//         controller: _pageController,
+//         onPageChanged: (index) {
+//           // When user swipes, update the notifier so the nav bar highlight follows
+//           selectedPageNotifer.value = index;
+//         },
+//         children: pages,
+//       ),
+//       bottomNavigationBar: NavbarWidget(),
+//     );
+//   }
+// }
